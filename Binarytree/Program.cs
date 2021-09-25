28b140efff4db5e5dfe5108b223a52c5ebd6649d
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-25 23:02:25 +0530

    Added : See Like Diagram

--- Binarytree/Program.cs
+++ Binarytree/Program.cs
@@ -10,7 +10,18 @@ namespace Binarytree
             BinarytreeList<int> binarytreeList = new BinarytreeList<int>();
             binarytreeList.Insert(56);
             binarytreeList.Insert(30);
+            binarytreeList.Insert(22);
+            binarytreeList.Insert(40);
+            binarytreeList.Insert(11);
+            binarytreeList.Insert(16);
+            binarytreeList.Insert(3);
+
             binarytreeList.Insert(70);
+            binarytreeList.Insert(60);
+            binarytreeList.Insert(95);
+            binarytreeList.Insert(65);
+            binarytreeList.Insert(63);
+            binarytreeList.Insert(67);
 
         }
     }
